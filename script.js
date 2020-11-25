83d7c5a37eba15f59ba3f8dce4fbb2239757688d
Cecilia Olivera <ceci.coding@gmail.com>
2020-11-25 02:00:05 +0100

    finished adding projects

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 window.addEventListener("hashchange", function () {
      window.scrollTo(window.scrollX, window.scrollY - 190);
 });
+
+const burger = document.querySelector('#menu-wraps');
+const toggler = document.querySelector('.toggler');
+
+burger.addEventListener('click', (e) => {
+     if (e.target.className === 'burger-list-item') {
+          toggler.checked = false;
+     }
+})
